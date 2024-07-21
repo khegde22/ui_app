bf36a27c54acdc9813feacc06ba07d88bc383fc2
khegde22 <karthikhegde22@gmail.com>
2024-07-21 11:19:12 +0530

    app updated for 10 companies

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import pandas as pd
 
+#Mapping dataframe
+company_name_df = pd.read_excel('company_name_map.xlsx')
+company_name_df = company_name_df.iloc[0:10,]
+
 # Set page layout to wide
 st.set_page_config(layout="wide")
 
 # Function to read Excel file and display table
 def display_summary(company_name):
+    scip_code_df = company_name_df[company_name_df['company_name']==company_name]
+    scrip_code = scip_code_df['scrip_code'].iloc[0]
     # Assuming you have predefined paths or a method to select the correct file based on company_name
-    file_path = f"{company_name}.xlsx"
+    file_path = f"./data/{scrip_code}.xlsx"
     
     # Read Excel file into DataFrame
     df = pd.read_excel(file_path)
@@ -20,7 +26,7 @@ def main():
     st.title('Concall Screener')
     
     # Company selection dropdown
-    company_name = st.selectbox('Select a company', ['comp1', 'comp2'])
+    company_name = st.selectbox('Select a company', company_name_df['company_name'].to_list())
     
     if company_name:
         display_summary(company_name)
